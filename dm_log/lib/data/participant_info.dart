6d4256826ac33fd07488cb8b082e052ae19519d8
jullouie <jljz2022@mymail.pomona.edu>
2024-04-16 18:23:55 -0700

    finish instance variables

--- dm_log/lib/data/participant_info.dart
+++ dm_log/lib/data/participant_info.dart
@@ -5,28 +5,39 @@ class ParticipantInfo {
   final String altId;
   final ParticipantType type; // Adding enum for type
 
-  // TODO: should this be final? be able to be changed
-  final StatusType status;
-  final String leftTag;
-  final String rightTag;
-  final int defaultLENA;
-  final int sonyID;
+  // not final because we want them to be able to changed
+  String shortID;
+  String leftTag;
+  String rightTag;
+  String defaultLENA;
+  String sonyID;
+  String lenaON;
+  String lenaOFF;
+  String vestON;
+  String vestOFF;
+  StatusType status;
+  String subjectID;
+  String notes;
 
+  // constructor with default values
   ParticipantInfo({
     required this.name,
     this.timeEntered = "00:00",
     this.altId = "",
     this.type = ParticipantType.Student, // Default type
-
-    // TODO: do i want this intialized default to present
-    this.status = StatusType.PRESENT,
+    this.shortID = "",
     this.leftTag = "",
     this.rightTag = "",
-    this.defaultLENA = 0,
-    this.sonyID = 0
+    this.defaultLENA = "",
+    this.sonyID = "",
+    this.lenaON = "",
+    this.lenaOFF = "",
+    this.vestON = "",
+    this.vestOFF = "",
+    this.status = StatusType.PRESENT,
+    this.subjectID = "",
+    this.notes="",
   });
-
-  // TODO: do i update this too w all the instance variables?
 
   /// Create a copy of this [ParticipantInfo] but with the given fields replaced with the new values.
   ParticipantInfo copyWith({
@@ -34,16 +45,40 @@ class ParticipantInfo {
     String? timeEntered,
     String? altId,
     ParticipantType? type,
+    String? shortID,
+    String? leftTag,
+    String? rightTag,
+    String? defaultLENA,
+    String? sonyID,
+    String? lenaON,
+    String? lenaOFF,
+    String? vestON,
+    String? vestOFF,
+    StatusType? status,
+    String? subjectID,
+    String? notes,
   }) {
     return ParticipantInfo(
       name: name ?? this.name,
       timeEntered: timeEntered ?? this.timeEntered,
       altId: altId ?? this.altId,
       type: type ?? this.type,
+      shortID: shortID ?? this.shortID,
+      leftTag: leftTag ?? this.leftTag,
+      rightTag: rightTag ?? this.rightTag,
+      defaultLENA: defaultLENA ?? this.defaultLENA,
+      sonyID: sonyID ?? this.sonyID,
+      lenaON: lenaON ?? this.lenaON,
+      lenaOFF: lenaOFF ?? this.lenaOFF,
+      vestON: vestON ?? this.vestON,
+      vestOFF: vestOFF ?? this.vestOFF,
+      status: status ?? this.status,
+      subjectID: subjectID ?? this.subjectID,
+      notes: notes ?? this.notes,
     );
   }
 
-  // TODO: do we want such a long toString
+  // TODO: finish toString
   @override
   String toString() {
     return '[name: $name, timeEntered: $timeEntered, altId: $altId, type: ${type.toString().split('.').last}, status: $status, left tag: $leftTag, right tag: $rightTag, default Lena: $defaultLENA, SONY ID: $sonyID]';
@@ -54,4 +89,5 @@ class ParticipantInfo {
 enum ParticipantType { Student, Adult }
 
 /// Defines the type of status.
-enum StatusType { PRESENT, ABSENT, NODATA}
+enum StatusType { PRESENT, ABSENT, NODATA }
+
