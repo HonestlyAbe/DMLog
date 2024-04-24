6aadeb7182e1f213e70c6ce227aed4a4078da09d
Sarah Yandell <syandell@g.hmc.edu>
2024-04-23 18:43:48 -0700

    added short ID, left and right tag, sony id, and subject ID (need to finsih and edit)

--- dm_log/lib/data/participant_info.dart
+++ dm_log/lib/data/participant_info.dart
@@ -17,6 +17,11 @@ class ParticipantInfo {
   String ULeft;
   String URight;
   String notes;
+  String subjectID;
+  String shortID;
+  String leftTag;
+  String rightTag;
+  String sonyID;
   final StudyInfo study;
   final ParticipantEntry provider;
   ParticipantType type; // Adding enum for typ
@@ -36,6 +41,11 @@ class ParticipantInfo {
     this.URight = "",
     this.notes = "",
     this.type = ParticipantType.student,
+    this.subjectID = "",
+    this.shortID = "",
+    this.leftTag = "",
+    this.rightTag = "",
+    this.sonyID = "",
   });
 
   /// Create a copy of this [ParticipantInfo] but with the given fields replaced with the new values.
@@ -52,6 +62,11 @@ class ParticipantInfo {
     String? ULeft,
     String? URight,
     ParticipantType? type,
+    String? subjectID,
+    String? shortID,
+    String? leftTag,
+    String? rightTag,
+    String? sonyID,
   }) {
     return ParticipantInfo(
       study: study,
@@ -68,12 +83,18 @@ class ParticipantInfo {
       ULeft: ULeft ?? this.ULeft,
       URight: URight ?? this.URight,
       type: type ?? this.type,
+      subjectID: subjectID ?? this.subjectID,
+      shortID: shortID ?? this.shortID,
+      leftTag: leftTag ?? this.leftTag,
+      rightTag: rightTag ?? this.rightTag,
+      sonyID: sonyID ?? this.sonyID,
+      
     );
   }
 
   @override
   String toString() {
-    return '[name: $name, timeEntered: $timeEntered, altId: $altId, type: ${type.toString().split('.').last}]';
+    return '[name: $name, timeEntered: $timeEntered, altId: $altId, subjectID: $subjectID,  shortID: $shortID, leftTag: $leftTag, rightTag: $rightTag , sonyID: $sonyID, type: ${type.toString().split('.').last}]';
   }
 }
 
